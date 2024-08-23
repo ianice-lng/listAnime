1db0e6cd1a309cfe3c01019303c31ae2a8fcb278
zaphir21 <149177599+zaphir21@users.noreply.github.com>
2024-08-23 16:58:43 +0200

    delete Random Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import {AnimeRandom} from "./animeRandom.jsx";
+// import {AnimeRandom} from "./animeRandom.jsx";
 import {AnimeDetail} from "./animeDetail.jsx";
 import {AnimeList} from "./animeList.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -10,7 +10,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/random" element={<AnimeRandom />} />
+        {/* <Route path="/random" element={<AnimeRandom />} /> */}
         <Route path="/anime/:id" element={<AnimeDetail />} />
         <Route path="/" element={<AnimeList />} />
       </Routes>
